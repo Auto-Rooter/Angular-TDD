19f33dd42177c87a6ee86f7d68d88856c69af950
Hadi <hadi.assalem@gmail.com>
2020-01-01 20:10:37 +0100

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { HomeComponent } from './src/components/home/home.component';
+import { HomesComponent } from './src/components/homes/homes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HomesComponent
   ],
   imports: [
     BrowserModule
